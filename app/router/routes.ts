c5198392e760f953c14f97706d41082bbac3c5a3
rodri <rodrigomenescal.rm@gmail.com>
2024-09-27 09:31:07 -0300

    fix: Corrige erro de cors

--- app/router/routes.ts
+++ app/router/routes.ts
@@ -4,11 +4,11 @@ import {PessoaController} from "../controller/PessoaController";
 const router = Router();
 const pessoaController = new PessoaController();
 
-router.post('/pessoa', (req, res) => pessoaController.createPessoa(req, res));
-router.get('/pessoas', (req, res) => pessoaController.getAllPessoas(req, res));
-router.get('/pessoa/:id', (req, res) => pessoaController.getPessoaById(req, res));
-router.put('/pessoa/:id', (req, res) => pessoaController.updatePessoa(req, res));
-router.delete('/pessoa/:id', (req, res) => pessoaController.deletePessoa(req, res));
+router.post('/usuario', (req, res) => pessoaController.createPessoa(req, res));
+router.get('/usuarios', (req, res) => pessoaController.getAllPessoas(req, res));
+router.get('/usuario/:id', (req, res) => pessoaController.getPessoaById(req, res));
+router.put('/usuario/:id', (req, res) => pessoaController.updatePessoa(req, res));
+router.delete('/usuario/:id', (req, res) => pessoaController.deletePessoa(req, res));
 
 
 export default router;
